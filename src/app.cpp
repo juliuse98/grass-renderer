a003555e1dcb174b969ee4c69c71069364657ca2
juliuse98 <91191144+juliuse98@users.noreply.github.com>
2023-05-19 20:47:53 +0200

    Add Keyboard/Mouse Input and Detect Resize

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,6 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
+#include "imgui/imconfig.h"
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
@@ -10,18 +11,26 @@
 #define WIDTH 960
 #define HEIGHT 540
 
+
+
 int main()
 {
+
 	GLFWwindow* window;
 
 	if (!glfwInit())
 	{
-		std::cout << "[ERROR]: Glew Initialization has failed." << std::endl;
+		const char* error;
+		glfwGetError(&error);
+		std::cout << "GLFW Initialization Error: " << error << std::endl;
 		return -1;
 	}
+
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
 	window = glfwCreateWindow(WIDTH, HEIGHT, "Grass Renderer", NULL, NULL);
 	if (!window)
@@ -31,34 +40,58 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
-
 	glfwSwapInterval(0);
 
 
-	if (glewInit() != GLEW_OK) {
+	if (glewInit() != GLEW_OK)
+	{
 		std::cout << "[ERROR]: Glew Initialization has failed." << std::endl;
 	}
 
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	{
-		std::cout << "Hello World!" << std::endl;
 		ImGui::CreateContext();
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
+
 		while (!glfwWindowShouldClose(window))
 		{
+			glfwPollEvents();
 			ImGui_ImplGlfwGL3_NewFrame();
+
+			if (ImGui::IsKeyPressed(GLFW_KEY_W)) {
+				std::cout << "W Key Pressed" << std::endl;
+			}
+			if (ImGui::IsKeyPressed(GLFW_KEY_A)) {
+				std::cout << "A Key Pressed" << std::endl;
+			}
+			if (ImGui::IsKeyPressed(GLFW_KEY_S)) {
+				std::cout << "S Key Pressed" << std::endl;
+			}
+			if (ImGui::IsKeyPressed(GLFW_KEY_D)) {
+				std::cout << "D Key Pressed" << std::endl;
+			}
+			if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape))) {
+				glfwSetWindowShouldClose(window, GLFW_TRUE);
+			}
+
+			auto pos = ImGui::GetMousePos();
+			pos.x = pos.x - WIDTH / 2;
+			pos.y = pos.y - HEIGHT / 2;
+			
 			ImGui::Begin("Test");
-			float x = 1;
-			ImGui::SliderFloat3("Translation A", &x, 0.0f, 960.0f);
+			std::string posString = "X: " + std::to_string(pos.x) + " Y: " + std::to_string(pos.y);
+			ImGui::Text(posString.c_str());
+			if(pos.x != 0)
+				std::cout << "Delta " << posString << std::endl;
+			glfwSetCursorPos(window, WIDTH / 2, HEIGHT / 2);
 			ImGui::End();
 
 			ImGui::Render();
 			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 			glfwSwapBuffers(window);
-			glfwPollEvents();
 		}
 	}
 	
